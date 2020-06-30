7d85393cf09eade9c53937c53d54f004ae0f27c1
Sifat-16 <59054820+Sifat-16@users.noreply.github.com>
2020-07-01 03:42:38 +0600

    Update urls.py
    Fixed urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.home, name="home"),
-    path('<slug:port_slug>', views.port, name="port")
+    path('port/<slug:port_slug>', views.port, name="port")
 ]
